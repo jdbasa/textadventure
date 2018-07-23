fb4002d2088a9b57965d1654d0ab89b59e3f8497
Luca <luca.duran@hotmail.com>
2018-07-23 14:09:23 -0700

    Add basic character methods

--- src/characters/character.cpp
+++ src/characters/character.cpp
@@ -1,1 +1,44 @@
+#include <algorithm>
 #include "character.h"
+
+using namespace std;
+
+int Character::get_level() {
+	return level;
+}
+
+int Character::get_health() {
+	return health;
+}
+
+int Character::deal(int damage) {
+	health = max(health - damage, 0);
+
+	return health;
+}
+
+int Character::gain(int heal) {
+	health = max(health + heal, max_health);
+
+	return health;
+}
+
+int Character::get_magic() {
+	return magic;
+}
+
+bool Character::consume(int mana) {
+	if (mana > magic) {
+		return false;
+	}
+
+	magic -= mana;
+	
+	return true;
+}
+
+int Character::regain(int mana) {
+	magic = min(magic + mana, max_magic);
+
+	return magic;
+}
--- src/headers/character.h
+++ src/headers/character.h
@@ -1,2 +1,20 @@
 class Character {
+public:
+	int get_level();
+
+	int get_health();
+	int deal(int damage);
+	int gain(int heal);
+
+	int get_magic();
+	bool consume(int mana);
+	int regain(int mana);
+private:
+	int level;
+
+	int max_health;
+	int health;
+
+	int max_magic;
+	int magic;
 };
